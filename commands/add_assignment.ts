3463ba4e9abf1aa96d557168a60d48db972a26db
Darahaas N <65232365+Darahaas2001@users.noreply.github.com>
2022-10-06 11:48:31 +0530

    Assignment Command

---
+++ commands/add_assignment.ts
@@ -0,0 +1,88 @@
+import WAWebJS from 'whatsapp-web.js';
+import Assignment from '../Models/assignment.model';
+import User from '../Models/user.model';
+import { fail } from '../utils/chalk';
+
+export default {
+	name: 'add_assignment',
+	type: 'faculty',
+	description: 'Creates a new assignment for students',
+	exec: async (
+		client: WAWebJS.Client,
+		message: WAWebJS.Message,
+		args: string[]
+	) => {
+		try {
+			let userData = await User.findOne({
+				mobileNo: (message.author as string).split('@')[0],
+			});
+			if (userData && userData.userType !== 'student') {
+				let matchedArr = message.body.split('||');
+				matchedArr.shift();
+
+				let assignmentData = await Assignment.findOne({
+					branch: matchedArr[0],
+					yearOfStudy: +matchedArr[1],
+					semester: +matchedArr[2],
+				});
+
+				if (!assignmentData) {
+					let obj = {
+						branch: matchedArr[0],
+						yearOfStudy: +matchedArr[1],
+						semester: +matchedArr[2],
+						section: matchedArr[3],
+						assignment: {
+							subjectName: matchedArr[4].toUpperCase(),
+							assignmentNo: +matchedArr[5],
+							deadLine: matchedArr[6],
+							description: matchedArr[7],
+							submittedStudents: [],
+						},
+					};
+					await Assignment.create(obj);
+					client.sendMessage(message.from, 'Assignment added successfully');
+				} else {
+					let assignmentData = await Assignment.findOne({
+						branch: matchedArr[0],
+						yearOfStudy: +matchedArr[1],
+						semester: +matchedArr[2],
+						assignment: {
+							$elemMatch: {
+								subjectName: matchedArr[4].toUpperCase(),
+								assignmentNo: +matchedArr[5],
+							},
+						},
+					});
+					if (!assignmentData) {
+						let obj = {
+							subjectName: matchedArr[4].toUpperCase(),
+							assignmentNo: +matchedArr[5],
+							deadLine: matchedArr[6],
+							description: matchedArr[7],
+							submittedStudents: [],
+						};
+						await Assignment.findOneAndUpdate(
+							{
+								branch: matchedArr[0],
+								yearOfStudy: +matchedArr[1],
+								semester: +matchedArr[2],
+							},
+							{ $push: { assignment: obj } }
+						);
+						client.sendMessage(message.from, 'Assignment added successfully');
+					} else {
+						client.sendMessage(message.from, 'Assignment exists already');
+					}
+				}
+			}
+		} catch (err) {
+			fail(err);
+			client.sendMessage(message.from, `${err}`);
+			client.sendMessage(
+				message.from,
+				'Error occured, please contact developer'
+			);
+		}
+	},
+};

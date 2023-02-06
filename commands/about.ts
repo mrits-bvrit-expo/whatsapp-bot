e09d5d2e820b830b82191253a79ff65c9c81f5fa
andeymandeep@gmail.com <andeymandeep@gmail.com>
2023-02-06 10:11:44 +0530

    Tested button functionality.

--- commands/about.ts
+++ commands/about.ts
@@ -1,5 +1,6 @@
 import WAWebJS from 'whatsapp-web.js';
 import { fail } from '../utils/chalk';
+import { List, Buttons } from 'whatsapp-web.js';
 
 export default {
 	name: 'about',
@@ -8,13 +9,15 @@ export default {
 	usage: '!about',
 	exec: (client: WAWebJS.Client, message: WAWebJS.Message, args: string[]) => {
 		try {
-			let startTime = process.hrtime();
-			let aboutData = `*Designed for Project Expo 2022 by MRITS* \n A.Mandeep \n M.Harshika \n M.Manaswini \n N.Darahaas \n\n Made with ❤️ using JS`;
-			let endTime = process.hrtime(startTime);
-			message.reply(
-				aboutData + `\ntook ${endTime[1]} nano-seconds`,
-				message.from
-			);
+			// let startTime = process.hrtime();
+			// let aboutData = `*Designed for Project Expo 2022 by MRITS* \n A.Mandeep \n M.Harshika \n M.Manaswini \n N.Darahaas \n\n Made with ❤️ using JS`;
+			// let endTime = process.hrtime(startTime);
+			let listData = new Buttons("This is body", [{body:"button1"},{body:"button2"}], null, null)
+			client.sendMessage(message.from, listData)
+			// message.reply(
+			// 	aboutData + `\ntook ${endTime[1]} nano-seconds`,
+			// 	message.from
+			// );
 		} catch (err) {
 			fail(err);
 			client.sendMessage(message.from, `${err}`);
